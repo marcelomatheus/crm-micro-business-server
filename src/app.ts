3d0d2d3fc90e2265937faed7c44b115c2b50d870
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-03-05 11:15:57 -0300

    feat: initialize Express server

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger";
+import authRouter from "./routes/auth-route";
+import "dotenv/config";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.use("/auth", authRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.listen(port, () => {
+  return console.log(`Server running at http://localhost:${port}`);
+});
