b86fbfa2d2fe8edfae9e8a185a1d2f48b9a9bd52
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-04-06 22:36:29 -0300

    docs: update swagger definition for accurate API documentation

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,21 +1,21 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
 const swaggerDefinition = {
-  openapi: "3.0.0",
   info: {
-    title: "Leads Register",
-    version: "1.0.0",
-    description: "Documentation for the Leads Register API",
     contact: {
-      name: "Marcelo Matheus",
       email: "marcelomatheusbr@gmai.com",
+      name: "Marcelo Matheus",
     },
+    description: "Documentation for the Customers Register API",
+    title: "Customers CRM",
+    version: "1.0.0",
   },
+  openapi: "3.0.0",
 };
 
 const options = {
-  definition: swaggerDefinition,
   apis: ["./src/routes/*.ts", "./src/routes/*.js"], // Path to the API routes in your Node.js application
+  definition: swaggerDefinition,
 };
 
 const swaggerSpec = swaggerJSDoc(options);
